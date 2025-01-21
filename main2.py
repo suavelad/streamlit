87fa503c5833befe4ad48f146dee1ac927483dd6
Sunday <sunnexajayi@gmail.com>
2025-01-21 11:55:45 +0100

    first commit

---
+++ main2.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import time
+
+st.write(
+#New App
+"Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+
+st.text_input("Your name", key="article")
+
+# You can access the value at any point with:
+article = st.session_state.article
+print(article)
+st.session_state.article
+
+
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+st.sidebar.checkbox("Select a mode",False)
+
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
